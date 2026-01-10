7511227343c011c6b0e8b7ca482aeb9fc9eb0d95
tanjaSimonji <tanja.simonji@outlook.com>
2026-01-10 16:45:16 +0100

    deleted files

--- sriptRepertoar.js
+++
@@ -1,8 +0,0 @@
-var cenaKarte = 400;
-const filmovi = ["Zlica", "Konklava", "Ovde", "Gladiator 2", "Exkurzija"];
-
-var cena = document.getElementsByClassName("cena");
-for (var i = 0; i < cena.length; i++) {
-    cena[i].textContent = "Cena karte: " + cenaKarte + "din";
-}
-
